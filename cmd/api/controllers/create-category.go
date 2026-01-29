c0cd3bf6844f1f55ffb219070798fccb50848ebb
Gabriel Eduardo de Moura Franca <gabrielfranca172@gmail.com>
2026-01-29 11:27:31 -0300

    controller

--- cmd/api/controllers/create-category.go
+++ cmd/api/controllers/create-category.go
@@ -4,7 +4,25 @@ import {
 	"github.com/gin-gon/gin"
 }
 
-func CreateCategory(ctx *gin.Context){
 
-	useCase := useCase
+type CreateCategoryInput struct{
+	Name string `json:"name" binding:"required"`
+}
+
+func CreateCategory(ctx *gin.Context){
+	var body CreateCategoryInput
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.AbortWithStatusJson(htpp.StatusBadRequest,
+		    gin.H{
+			  "sucess": false,
+			  "error": err,Error(),
+		})
+	return 
+
+	}
+
+	useCase := use_cases.NewCreateCategoryUseCase{}
+
+	useCase.Execute()
 }
