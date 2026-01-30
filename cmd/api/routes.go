0a6b22650d418cbe1a909caef5e5518ccada2aab
Gabriel Eduardo de Moura Franca <gabrielfranca172@gmail.com>
2026-01-30 11:47:16 -0300

    xd

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/gabrielfranca42/go-microservices/cmd/api/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func CategoryRoutes(route *gin.Engine) {
-	CategoryRoutes := route.Group("/")
+	CategoryRoutes := route.Group("/categories")
+
+	CategoryRoutes.POST("/", controllers.CreateCategory)
 }
