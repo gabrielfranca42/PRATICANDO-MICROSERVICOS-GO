dd80cd0697c4fed3c3ff3ec5e3e4d7c4fff340da
Gabriel Eduardo de Moura Franca <gabrielfranca172@gmail.com>
2026-02-03 08:35:59 -0300

    in memory feito

--- internal/entities_models/repository/in-memory-repository.go
+++ internal/entities_models/repository/in-memory-repository.go
@@ -1,7 +1,19 @@
 package repository
 
-import "github.com/gabrielfranca42/go-microservices/internal/entities_models"
+import entitiesmodels "github.com/gabrielfranca42/go-microservices/internal/entities_models"
 
-type CategoryRepository struct {
-	db []*entities_models.Category
+type inMemoryCategoryRepository struct {
+	db []*entitiesmodels.Category
+}
+
+func NewInMemoryCategoryRepository() *inMemoryCategoryRepository {
+	return &inMemoryCategoryRepository{
+		db: make([]*entitiesmodels.Category, 0),
+	}
+}
+
+func (r *inMemoryCategoryRepository) Save(category *entitiesmodels.Category) error {
+	r.db = append(r.db, category)
+
+	return nil
 }
